f12615c8d45d4dab4beeefac8fd433b7cd777c88
Piotr Stapp <piotr.stapp@finai.com>
2019-04-07 11:04:05 +0200

    Progress bar

---
+++ src/app/bar/bar.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Input, ViewChild, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  templateUrl: './bar.component.html',
+  styleUrls: ['./bar.component.css']
+})
+export class BarComponent implements OnInit {
+
+  @ViewChild('container') container;
+
+  private _percentage: number;
+  private _updatable: boolean = false;
+
+  private _updating: boolean = false;
+
+
+
+  constructor() { }
+
+  @Input('percentage')
+  set percentage(value: number) {
+    this._percentage = Math.max(0, Math.min(100, value));
+  }
+
+  @Input('updatable')
+  set updatable(value: boolean) {
+    this._updatable = value;
+  }
+
+  mousedown(){
+    this._updating = true;
+  }
+
+  mouseup(){
+    this._updating = false;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  update(event:any){
+    if(!this._updatable)
+      return;
+    if(!this._updating)
+      return
+    if(this.container.nativeElement.offsetWidth!=0){
+      this.percentage = 
+        Math.floor(event.clientX/this.container.nativeElement.offsetWidth*100);
+    }
+  }
+  
+  ngOnInit() {
+  }
+
+}
